e5fba0592624940e5221e9d77529609a7f29325d
Brijendra Pratap <brijendraoffice@gmail.com>
2024-03-19 11:26:50 +0530

    tdyr

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
             <p>SHOPPER</p>
         </div>
         <ul className="nav-menu">
-            <li onClick={()=> {setMenu("shop")}}>sex {menu==="shop"? <hr/>:<></>}</li>
+            <li onClick={()=> {setMenu("shop")}}>Shop {menu==="shop"? <hr/>:<></>}</li>
             <li onClick={()=> {setMenu("mens")}}>Men {menu=== "mens"? <hr/>:<></>}</li>
             <li onClick={()=> {setMenu("womens")}}>Women {menu=== "womens"? <hr/>:<></>}</li>
             <li onClick={()=> {setMenu("kids")}}>Kids {menu==="kids"? <hr/>:<></>}</li>
