dd3263f83043f4abdf8d76ce6fb646419729831b
Brijendra Pratap <brijendraoffice@gmail.com>
2024-03-19 11:20:40 +0530

    jka

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <div>Hello</div>
+    </div>
+  );
+}
+
+export default App;
